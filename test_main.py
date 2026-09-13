44513a25cba29d80e621f2c61d212422cc2447c4
builtbyaqsa <iaqsaiqbal113@gmail.com>
2026-09-13 20:22:28 +0500

    fix: update test suite to match active endpoints

--- test_main.py
+++ test_main.py
@@ -3,10 +3,6 @@ from app import app
 
 client = TestClient(app)
 
-def test_read_root():
-    response = client.get('/')
-    assert response.status_code == 200
-
 def test_get_scans_endpoint():
     response = client.get('/scans')
     assert response.status_code == 200
